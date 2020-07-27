ab82fb4cbac91cf90e065efc6fc64f33ae6c584c
wynnja <wynnbsmith98@gmail.com>
2020-07-27 00:18:33 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Simple Edit for VCS
         </a>
       </header>
     </div>
